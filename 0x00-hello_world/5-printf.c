5191477a8ef675c8556bc5b64d4984d02a525a61
takudzwarusike <trusike69@gmaail.com>
2022-09-08 09:23:28 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
