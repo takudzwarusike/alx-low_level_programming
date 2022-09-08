3122c985790cebb5e4f23e8e2915782c6165550e
takudzwarusike <trusike69@gmaail.com>
2022-09-08 12:22:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,25 @@
-#include<stdio.h>
+nclude <stdio.h>
 
-int main()
 
+
+/**
+ *
+ * main - Printing
+ *
+ * *
+ *
+ * Return: Always 0 (Successful)
+ *
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}
