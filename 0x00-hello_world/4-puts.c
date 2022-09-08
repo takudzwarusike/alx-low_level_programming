a516c95a28144b27dd7c74a8e6c9520289b9284a
takudzwarusike <trusike69@gmaail.com>
2022-09-08 12:58:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
-nclude <stdio.h>
-
-
+#include <studio.>
 
 /**
- *
- * main - Printing
- *
- * *
+ * main - print the string in the put function
  *
  * Return: Always 0 (Successful)
- *
  */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
