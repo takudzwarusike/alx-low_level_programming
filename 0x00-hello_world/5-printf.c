ce93f29699c382c3cc4d3c2c20d3e375c2fd762d
takudzwarusike <trusike69@gmaail.com>
2022-09-09 02:00:09 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Prints a text
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
-
-int main(void)
- {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
- }
+int main (void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n")';
+	return (0);
+}
